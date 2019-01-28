bf4bd726eeadc603cfe69f8ccaf959a130845c4d
Gabriel <gabrielmqs04@gmail.com>
2019-01-28 19:25:16 -0200

    melhorando a documentação do modulo media e da classe Movie

--- media.py
+++ media.py
@@ -3,9 +3,20 @@
 import webbrowser
 
 """
-    Classe responsável por encapsular as informações de um filme
+    Modulo responsável por tratar e encapsular a classe Movie.
 """
 class Movie():
+    """
+        Classe utiliziada para criar objetos com as informações de um filme.
+
+        Atributtes:
+            title (str): Titulo do filme.
+            poster_image_url (str): Url que contém a imagem do poster do filme.
+            trailer_youtube_url (str): Url do trailer do filme no youtube.
+            director (str): Nome do diretor do filme.
+            release_date (str): Data de lançamento do filme.
+            runtime (str): Tempo de duração do filme.
+    """
 
     def __init__(self, title, poster_image_url,
                  trailer_youtube_url, director,
@@ -17,6 +28,6 @@ class Movie():
         self.release_date = release_date
         self.runtime = runtime
 
-    # função responsável por exibir o trailer do filme
     def show_trailer(self):
+        """Função responsável por exibir o trailer do filme."""
         webbrowser.open(self.trailer_youtube_url)
